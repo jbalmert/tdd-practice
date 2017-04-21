d713d5958d4e8eb9e6dc0a4e1f484fd4944e3c1e
jbalmert <morpheus-github@elaychbee.com>
2017-04-21 08:04:03 -0400

    Created initial failing end to end test to guide the main deveopment.

---
+++ src/main/java/com/tdd/Game.java
@@ -0,0 +1,7 @@
+package com.tdd;
+
+public class Game {
+    public void move(int position) {
+
+    }
+}
---
+++ src/test/java/com/tdd/TicTacToeEndToEndTest.java
@@ -0,0 +1,31 @@
+package com.tdd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.io.PrintStream;
+
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TicTacToeEndToEndTest {
+
+    @Mock
+    PrintStream printer;
+
+    @Test
+    public void playerXWins() throws Exception {
+        Game game = new Game();
+
+        game.move(1);
+        game.move(5);
+        game.move(2);
+        game.move(9);
+        game.move(3);
+
+        verify(printer).println("Player X wins");
+
+    }
+}
