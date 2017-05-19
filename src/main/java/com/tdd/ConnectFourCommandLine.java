760b075fd90e5419537a6d6a433233d9790eacf0
jbalmert <morpheus-github@elaychbee.com>
2017-05-19 19:04:23 -0400

    Created initial end to end test.  It will continue to fail until we drive out the initial skeleton of the system.

---
+++ src/main/java/com/tdd/ConnectFourCommandLine.java
@@ -0,0 +1,15 @@
+package com.tdd;
+
+import java.io.BufferedReader;
+
+public class ConnectFourCommandLine {
+    private BufferedReader input;
+
+    public ConnectFourCommandLine(BufferedReader input) {
+        this.input = input;
+    }
+
+    public void acceptNextMove() {
+
+    }
+}
---
+++ src/test/java/com/tdd/ConnectFourEndToEndTest.java
@@ -0,0 +1,29 @@
+package com.tdd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.io.BufferedReader;
+import java.io.PrintStream;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ConnectFourEndToEndTest {
+
+    @Mock BufferedReader input;
+    @Mock PrintStream output;
+
+    @Test
+    public void player1PlacesMarkerInColumn() throws Exception {
+        when(input.readLine()).thenReturn("3");
+        ConnectFourCommandLine cli = new ConnectFourCommandLine(input);
+
+        cli.acceptNextMove();
+
+        verify(output).println("P1,3,0");
+    }
+}
